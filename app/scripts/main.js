a5981fde5011a1adf45e55b83ade7a22c76ed2e9
Ganesh Balaji <ganeshkbalaji@ymail.com>
2016-02-17 16:42:14 -0800

    fixed solution live coding uber interview front end

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,13 @@
-console.log('\'Allo \'Allo!');
+var count = 0;
+  function progress(percent, $element) {
+  var progressBarWidth = percent * $element.width() / 100;
+  $element.find('div').animate({width: progressBarWidth}, 5000).html(percent + "%");
+  };
+
+// progress(100, $("#progressBar"));
+
+$('#addBar').click(function() {
+  $(".container").append('<div id="progressBar'+count+'"><div></div></div>');
+  progress(100, $("#progressBar"+count));
+  count++;
+})
